a8d2f75bc06c4c7acac993a8efde7faa3c932dd8
kimanithuo1 <jkimanithuo@gmail.com>
2025-07-12 17:36:12 +0300

    added a simple interest calculator on the directory

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,15 @@
+#Defining variables
+
+principal = 1000
+
+rate = 0.05
+
+time = 3
+
+#Calculating simple interest
+
+interest = principal * rate * time
+
+#Print the results
+
+print(f"Simple Interest is: {interest}")

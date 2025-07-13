8757141b36fb9415ae0b1732381c49710ea063b2
kimanithuo1 <jkimanithuo@gmail.com>
2025-07-13 09:26:34 +0300

    created basic arithmetic python script for future_age_calculator.py

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,11 @@
+#Request user to input a current age
+
+current_age = int(input("How old are you?"))
+
+#How old will the user be in 2050 assuming we are in 2023
+
+future_age = current_age + 27
+
+#Print the results
+
+print(f"In 2050, you will be {future_age} years old.")

0542d005b1137b48b57c24d2255a2c3fa9fb6b7f
kimanithuo1 <jkimanithuo@gmail.com>
2025-07-20 20:22:35 +0300

    edited for script to generate multiplication table

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,7 +1,7 @@
-#enter a number to print its multiplication table
+# input number
 
 number = int(input("Enter a number to see its multiplication table: "))
 
-#Print the multiplication table
+# Print the multiplication table
 for i in range(1, 11):
-    print(f"The multiplication table of {number} is: {number} x {i} = {number * i}")
+    print(f"{number} * {i} = {number * i}")

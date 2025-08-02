de037fc238c73feab144837245a5f06c5d96b74d
kimanithuo1 <jkimanithuo@gmail.com>
2025-08-02 15:32:51 +0300

    changed wording in the test script for addition and substraction

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -9,12 +9,12 @@ class TestSimpleCalculator(unittest.TestCase):
         self.calc = SimpleCalculator()
 
 # Test methods
-    def test_add(self):
+    def test_addition(self):
         self.assertEqual(self.calc.add(3, 2), 5)
         self.assertEqual(self.calc.add(-1, -1), -2)
         self.assertEqual(self.calc.add(-1, 1), 0)
 
-    def test_subtract(self):
+    def test_subtraction(self):
         self.assertEqual(self.calc.subtract(5, 3), 2)
         self.assertEqual(self.calc.subtract(0, 5), -5)
 
@@ -22,7 +22,7 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.multiply(4, 3), 12)
         self.assertEqual(self.calc.multiply(-2, 3), -6)
 
-    def test_divide(self):
+    def test_division(self):
         self.assertEqual(self.calc.divide(10, 2), 5)
         self.assertEqual(self.calc.divide(9, 3), 3)
         self.assertIsNone(self.calc.divide(5, 0))  # Test divide by zero
